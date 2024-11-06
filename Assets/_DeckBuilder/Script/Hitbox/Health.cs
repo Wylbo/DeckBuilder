2cc370cbf2cbafe0bb07c42048a730270ca16884
Maximilien GALEA <maximilien.galea@gmail.com>
2024-11-07 00:50:10 +0100

    add health tracker

--- Assets/_DeckBuilder/Script/Hitbox/Health.cs
+++ Assets/_DeckBuilder/Script/Hitbox/Health.cs
@@ -5,9 +5,16 @@ public class Health : MonoBehaviour
 {
     [SerializeField] private int maxHealth;
 
-    private int CurrentHealth;
+    private int currentHealth;
+
+    public int MaxHealth => maxHealth;
 
     public event UnityAction On_Empty;
+
+    /// <summary>
+    /// send prev and new values
+    /// </summary>
+    public event UnityAction<int, int> On_Change;
 
     private void OnEnable()
     {
@@ -16,15 +23,21 @@ public class Health : MonoBehaviour
 
     public void Initialize()
     {
-        CurrentHealth = maxHealth;
+        currentHealth = maxHealth;
+        On_Change?.Invoke(currentHealth, currentHealth);
     }
 
     public void AddOrRemoveHealth(int damage)
     {
-        CurrentHealth += damage;
-        CurrentHealth = Mathf.Clamp(CurrentHealth, 0, maxHealth);
-        Debug.Log($"{name} new health:" + CurrentHealth);
-        if (CurrentHealth <= 0)
+        int previousValue = currentHealth;
+        currentHealth += damage;
+        currentHealth = Mathf.Clamp(currentHealth, 0, maxHealth);
+
+        On_Change?.Invoke(previousValue, currentHealth);
+
+        Debug.Log($"{name} new health:" + currentHealth);
+
+        if (currentHealth <= 0)
             On_Empty?.Invoke();
     }
 }
