7b51528a97f94d08955f39ea3b72db06d83e998b
Maximilien <maximilien.galea@gmail.com>
2025-11-02 20:57:00 +0100

    add Material property ref

--- Assets/_DeckBuilder/Script/Entity/CharacterVisual.cs
+++ Assets/_DeckBuilder/Script/Entity/CharacterVisual.cs
@@ -3,7 +3,8 @@ using UnityEngine;
 
 public class CharacterVisual : MonoBehaviour
 {
-    [SerializeField] private Renderer[] renderers;
+    [SerializeField] private MaterialPropertyRef[] DissolveProperties;
+
     [SerializeField] private float dissolveDuration = 1f;
     [SerializeField] private float waitBeforeDissolve = 0.5f;
     [SerializeField] private AnimationCurve dissolveCurve = AnimationCurve.Linear(0, 0, 1, 1);
@@ -16,17 +17,15 @@ public class CharacterVisual : MonoBehaviour
     IEnumerator DissolveCoroutine()
     {
         yield return new WaitForSeconds(waitBeforeDissolve);
-        MaterialPropertyBlock block = new MaterialPropertyBlock();
         float elapsed = 0f;
         while (elapsed < dissolveDuration)
         {
             elapsed += Time.deltaTime;
             float dissolveAmount = Mathf.Clamp01(elapsed / dissolveDuration);
-            block.SetFloat("_Dissolve", dissolveCurve.Evaluate(dissolveAmount));
 
-            foreach (var renderer in renderers)
+            foreach (var property in DissolveProperties)
             {
-                renderer.SetPropertyBlock(block);
+                property.SetValue(dissolveCurve.Evaluate(dissolveAmount));
             }
 
             yield return null;
