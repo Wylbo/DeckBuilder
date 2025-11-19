347d8471b7a72f25e3f30f437ae090d9320fa301
Maximilien Galea <maximilien.galea@gmail.com>
2025-11-19 14:28:36 +0100

    Refactor enemy pooling with PoolManager

--- Assets/_DeckBuilder/Script/Utils/PoolManager.cs
+++ Assets/_DeckBuilder/Script/Utils/PoolManager.cs
@@ -1,108 +1,160 @@
 using System.Collections.Generic;
-using System.Linq;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Pool;
 
 public class PoolManager : MonoBehaviour
 {
-	private class Pool
-	{
-		public int Hash { get; private set; }
+    private class Pool
+    {
+        public Pool(GameObject prefab, PoolType type)
+        {
+            Prefab = prefab;
+            Type = type;
+            Hash = prefab.name.GetHashCode();
+            ObjectPool = new ObjectPool<GameObject>(CreateInstance, OnGet, OnRelease, DestroyInstance, false);
+        }
 
-		public List<GameObject> AvailableObject { get; set; }
-		public PoolType Type { get; private set; }
+        public int Hash { get; }
+        public PoolType Type { get; }
+        public GameObject Prefab { get; }
+        public IObjectPool<GameObject> ObjectPool { get; }
 
-		public Pool(int hash, PoolType type = PoolType.GameObject)
-		{
-			Hash = hash;
-			AvailableObject = new List<GameObject>();
-			Type = type;
-		}
-	}
+        private GameObject CreateInstance()
+        {
+            Transform parent = GetParentToSpawnIn(Type);
+            GameObject instance = Object.Instantiate(Prefab, parent);
+            instance.name = Prefab.name;
+            instance.SetActive(false);
+            return instance;
+        }
 
+        private void OnGet(GameObject obj)
+        {
+            obj.SetActive(true);
+        }
 
-	public enum PoolType
-	{
-		VFX,
-		GameObject,
-	}
+        private void OnRelease(GameObject obj)
+        {
+            Transform parent = GetParentToSpawnIn(Type);
+            obj.transform.SetParent(parent, false);
+            obj.SetActive(false);
+        }
 
-	private static Dictionary<PoolType, GameObject> PoolTypeParents = new Dictionary<PoolType, GameObject>();
-	private static List<Pool> pools = new List<Pool>();
+        private static void DestroyInstance(GameObject obj)
+        {
+            if (obj != null)
+            {
+                Object.Destroy(obj);
+            }
+        }
+    }
 
-	private void Awake()
-	{
-		PoolTypeParents = new Dictionary<PoolType, GameObject>();
-		GameObject go;
-		foreach (PoolType type in EnumUtils.GetValues<PoolType>())
-		{
-			go = new GameObject(type.ToString());
-			PoolTypeParents.Add(type, go);
-			go.transform.parent = transform;
-		}
-	}
+    public enum PoolType
+    {
+        VFX,
+        GameObject,
+    }
 
-	public static T Provide<T>(GameObject obj, Vector3 position, Quaternion rotation, Transform attachedTo = null, PoolType type = PoolType.GameObject)
-	{
-		return Provide(obj, position, rotation, attachedTo, type).GetComponent<T>();
-	}
+    private static PoolManager instance;
+    private static readonly Dictionary<PoolType, GameObject> PoolTypeParents = new Dictionary<PoolType, GameObject>();
+    private static readonly Dictionary<int, Pool> pools = new Dictionary<int, Pool>();
 
-	public static GameObject Provide(GameObject gameObject, Vector3 position, Quaternion rotation, Transform attachedTo = null, PoolType type = PoolType.GameObject)
-	{
-		Pool pool = pools.Find(p => p.Hash == gameObject.name.GetHashCode());
+    private void Awake()
+    {
+        instance = this;
+        PoolTypeParents.Clear();
+        foreach (PoolType type in EnumUtils.GetValues<PoolType>())
+        {
+            GameObject go = new GameObject(type.ToString());
+            go.transform.SetParent(transform);
+            PoolTypeParents[type] = go;
+        }
+    }
 
-		if (pool == null)
-		{
-			pool = new Pool(gameObject.name.GetHashCode(), type);
-			pools.Add(pool);
-		}
+    private void OnDestroy()
+    {
+        if (instance == this)
+        {
+            instance = null;
+        }
+    }
 
-		GameObject providedObj = pool.AvailableObject.FirstOrDefault();
+    public static T Provide<T>(GameObject obj, Vector3 position, Quaternion rotation, Transform attachedTo = null, PoolType type = PoolType.GameObject)
+    {
+        GameObject provided = Provide(obj, position, rotation, attachedTo, type);
+        return provided != null ? provided.GetComponent<T>() : default;
+    }
 
-		if (providedObj == null)
-		{
-			providedObj = Instantiate(gameObject, position, rotation, attachedTo == null ? GetParentToSpawnIn(type) : attachedTo);
-			providedObj.name = gameObject.name;
-		}
-		else
-		{
-			providedObj.transform.SetPositionAndRotation(position, rotation);
-			providedObj.SetActive(true);
+    public static GameObject Provide(GameObject prefab, Vector3 position, Quaternion rotation, Transform attachedTo = null, PoolType type = PoolType.GameObject)
+    {
+        if (prefab == null)
+        {
+            Debug.LogError("Cannot provide from a null prefab");
+            return null;
+        }
 
-			if (attachedTo != null)
-				providedObj.transform.parent = attachedTo.transform;
+        Pool pool = GetOrCreatePool(prefab, type);
+        GameObject instance = pool.ObjectPool.Get();
 
-			pool.AvailableObject.Remove(providedObj);
-		}
+        Transform parent = attachedTo != null ? attachedTo : GetParentToSpawnIn(type);
+        instance.transform.SetParent(parent, false);
 
-		return providedObj;
-	}
+        instance.transform.SetPositionAndRotation(position, rotation);
+        return instance;
+    }
 
-	public static void Release(GameObject gameObject)
-	{
-		Pool pool = pools.Find(p => p.Hash == gameObject.name.GetHashCode());
+    public static void Release(GameObject gameObject)
+    {
+        if (gameObject == null)
+        {
+            return;
+        }
 
-		if (pool == null)
-		{
-			Debug.LogError($"Trying to release an unpooled object {gameObject.name}. Object is destroyed instead");
-			Destroy(gameObject);
-			return;
-		}
+        int hash = gameObject.name.GetHashCode();
+        if (!pools.TryGetValue(hash, out Pool pool))
+        {
+            Debug.LogError($"Trying to release an unpooled object {gameObject.name}. Object is destroyed instead");
+            Destroy(gameObject);
+            return;
+        }
 
-		gameObject.transform.parent = GetParentToSpawnIn(pool.Type);
-		gameObject.SetActive(false);
+        pool.ObjectPool.Release(gameObject);
+    }
 
-		pool.AvailableObject.Add(gameObject);
-	}
+    private static Pool GetOrCreatePool(GameObject prefab, PoolType type)
+    {
+        int hash = prefab.name.GetHashCode();
+        if (pools.TryGetValue(hash, out Pool pool))
+        {
+            if (pool.Type != type)
+            {
+                Debug.LogWarning($"[{nameof(PoolManager)}] Pool for {prefab.name} already exists with type {pool.Type}, requested type {type}");
+            }
 
-	private static Transform GetParentToSpawnIn(PoolType type)
-	{
-		if (PoolTypeParents.TryGetValue(type, out GameObject parent))
-		{
-			return parent.transform;
-		}
+            return pool;
+        }
 
-		return null;
+        pool = new Pool(prefab, type);
+        pools.Add(hash, pool);
+        return pool;
+    }
 
-	}
+    private static Transform GetParentToSpawnIn(PoolType type)
+    {
+        if (PoolTypeParents.TryGetValue(type, out GameObject parent) && parent != null)
+        {
+            return parent.transform;
+        }
+
+        if (instance == null)
+        {
+            return null;
+        }
+
+        GameObject go = new GameObject(type.ToString());
+        go.transform.SetParent(instance.transform);
+        PoolTypeParents[type] = go;
+        return go.transform;
+    }
 }
