7300f5b0ec2bd72d47c9b7d67823ce544e68fff6
Maximilien <maximilien.galea@gmail.com>
2025-11-29 17:47:41 +0100

    cleanup

--- Assets/_DeckBuilder/Script/Ability/AbilityBehaviours/AbilityBehaviourContext.cs
+++ Assets/_DeckBuilder/Script/Ability/AbilityBehaviours/AbilityBehaviourContext.cs
@@ -2,17 +2,17 @@ using UnityEngine;
 
 public sealed class AbilityBehaviourContext
 {
-	public Ability Ability { get; }
-	public AbilityCaster Caster { get; }
-	public Movement Movement { get; }
+    public Ability Ability { get; }
+    public AbilityCaster Caster { get; }
+    public Movement Movement { get; }
 
-	public ProjectileLauncher ProjectileLauncher => Caster.ProjectileLauncher;
-	public AbilityModifierManager ModifierManager => Caster.ModifierManager;
+    public ProjectileLauncher ProjectileLauncher => Caster.ProjectileLauncher;
+    public AbilityModifierManager ModifierManager => Caster.ModifierManager;
 
-	public AbilityBehaviourContext(Ability ability, AbilityCaster caster, Movement movement)
-	{
-		Ability = ability;
-		Caster = caster;
-		Movement = movement;
-	}
+    public AbilityBehaviourContext(Ability ability, AbilityCaster caster, Movement movement)
+    {
+        Ability = ability;
+        Caster = caster;
+        Movement = movement;
+    }
 }
